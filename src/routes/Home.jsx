cbda4ef4015b56b1ef3289c88fa5e38dc8424276
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-11-08 16:03:00 -0300

    feat: url base on axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
+import blogFetch from '../axios/config'
+
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import { Link } from 'react-router-dom'
 
@@ -10,9 +11,7 @@ const Home = () => {
 
   const getPosts = async () => {
     try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/posts'
-      )
+      const response = await blogFetch.get('/posts')
       const data = response.data
       setPosts(data)
     } catch (error) {
